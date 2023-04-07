a7c5b571eb89f7e11ba7d3a493cca851fea2e1ac
yianxd <jean4520rodriguez@gmail.com>
2023-04-06 19:13:14 -0500

    nada

--- angelo/a.py
+++ angelo/a.py
@@ -2,3 +2,5 @@ import sqlite3
 
 with sqlite3.connect('proyecto.sqbpro') as a:
     c=a.cursor()
+
+    
---
+++ jean/a.py
@@ -0,0 +1,4 @@
+import sqlite3
+
+with sqlite3.connect('debe(1).db') as a:
+    c=a.cursor()
