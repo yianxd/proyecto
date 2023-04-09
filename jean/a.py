057a7322a28821bcfb32346f720a73fb52bfc336
yianxd <jean4520rodriguez@gmail.com>
2023-04-09 12:38:11 -0500

    agregar

--- jean/a.py
+++ jean/a.py
@@ -1,6 +1,17 @@
 import sqlite3 
-
 with sqlite3.connect('proyecto.db') as con:
+    cursor=con.cursor()
 
-def agregar():
-    print("a")
+def agregar(conexion,id_tip,nombre_usu,apellido_usu,email_usu,telefono_usu,direccion_usu,contraseña_usu):
+    cur=conexion.cursor()
+    sentencia=f"INSERT INTO usuarios (id_tip,nombre_usu,apellido_usu,email_usu,telefono_usu,direccion_usu,contraseña_usu) VALUES ({id_tip},'{nombre_usu}','{apellido_usu}','{email_usu}','{telefono_usu}','{direccion_usu}','{contraseña_usu}')"
+    cur.execute(sentencia)
+    print("registro logrado con exito!")
+    sentencia=f"SELECT * FROM usuarios WHERE telefono_usu='{telefono_usu}'"
+    print(cur.execute(sentencia).fetchone())
+    
+
+agregar(con, 1, "juan", "calles", "adsf@gmail.com", "343-134-334", "paaaaaaa", "AKj123")
+#DELETE FROM Customers WHERE CustomerName='Alfreds Futterkiste';
+def eliminar(conexion,condicion1,operador,condicion2):
+    print("terminar esto")
