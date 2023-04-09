0f822fc4822d379b115db3898d4f485c9df3bca9
angelo <angelorizo84@gmai.com>
2023-04-09 18:29:07 -0500

    Revisar

--- angelo/clases_hab.py
+++ angelo/clases_hab.py
@@ -1,31 +1,11 @@
-class habitaciones:
+class habitacione:
     
     def __init__(self,id_hab,tipo_tha,descripcion_hab):
         self.__id_hab=id_hab
         self.tipo_than=tipo_tha
-        self.descripcion_hab=descripcion_hab
-    
-    def agregar_hab(self,tipo_tha,descripcion_tha):
-        self.tipo_than=tipo_tha
-        self.descripcion_hab=descripcion_tha    
+        self.descripcion_hab=descripcion_hab 
     
     def consultar_hab(self,id_hab,tipo_tha,descripcion_hab):
         self.__id_hab=id_hab
         self.tipo_than=tipo_tha
         self.descripcion_hab=descripcion_hab
-    
-    def modificar_tip(self,id_hab,tipo_hab):
-        self.__id_hab=id_hab
-        self.tipo_hab=tipo_hab
-    
-    def modificar_des(self,id_hab,descripcion_hab):
-        self.__id_hab=id_hab
-        self.descripcion_hab=descripcion_hab
-    
-    def modificar_tod(self,id_hab,tipo_hab,descripcion_hab):
-        self.__id_hab=id_hab
-        self.tipo_hab=tipo_hab
-        self.descripcion_hab=descripcion_hab
-    
-    def eliminar_hab(self,id_hab):
-        self.__id_hab=id_hab
