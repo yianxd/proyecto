68fa8a8381d2cc65edd51b2936b54b65be3229ae
Victorpri <vimapri1234@gmail.com>
2023-04-09 17:52:34 -0500

    CRUD y menu

--- victor/CRUD_factura.py
+++ victor/CRUD_factura.py
@@ -36,3 +36,71 @@ def modificar_total(c, Id, total):
     c.execute(sentencia)
     a.commit()
     print('Actualizacion completada con exito')
+
+def menu_cuenta():
+
+    print("Bienvenido al menu de la cuenta en que le podemos colaborar? ")
+    while True:
+        print("1. Agregar una factura")
+        print("2. Consultar una factura")
+        print("3. Eliminar una factura")
+        print("4. Modificar el total")
+        print("5. Salir")
+        eleccion=int(input("Seleccione la opcion: "))
+
+        if eleccion == 1:
+            archivo=open("historial_res.csv","a")
+            id_fac=int(input("Ingrese el Id de la factura"))
+            id_cue=int(input("Ingrese el Id de la cuenta"))
+            total_fac=int(input("Ingrese el total de la factura"))
+            archivo.write("Agregacion\n")
+            archivo.write("Id factura: ",id_fac,"\n")
+            archivo.write("Id cuenta: ",id_cue,"\n")
+            archivo.write("Total a pagar: ",total_fac,"\n")
+            archivo.close()
+            agregar_factura(c, id_fac, id_cue, total_fac)
+
+
+        elif eleccion == 2:
+            while True:
+                print("1. Consultar una factura")
+                print("2. Consultar todo")
+                print("3. salir")
+                opcion=int(input("Escriba el numero de su eleccion: "))
+
+                if opcion == 1:
+                    Id= int(input("Escriba el id de la factura: "))
+                    consultar_factura(c,Id)
+
+                elif opcion == 2:
+                    consultar_todo(c)
+
+                elif opcion == 3:
+                    menu_cuenta()
+
+
+        elif eleccion == 3:
+            archivo= open("historial_res.csv","a")
+            Id=int(input("Escriba la Id de la factura la cualquiere eliminar: "))
+            archivo.write("Eliminacion de factura")
+            archivo.write("Id: ",Id,"\n")
+            archivo.close()
+            eliminar_facturas(c, Id)
+
+        
+        elif eleccion == 4:
+            archivo=open("historial_res.csv","a")
+            Id=int(input("Escriba el Id e la cuenta la cual va a modificar: "))
+            total=int(input("Escriba el nuevo total: "))
+            archivo.write("Modificacion de el total")
+            archivo.write("id: ",Id,",")
+            archivo.write("Fecha_llegada: ",total,"\n")
+            archivo.close()
+            modificar_total(c, Id, total)
+
+
+        elif eleccion == 5:
+            exit()
+
+menu_cuenta()
+          
