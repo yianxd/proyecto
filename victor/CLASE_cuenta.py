a8f3c540d3b74b302a2a49d9c07e38d774224608
Victorpri <vimapri1234@gmail.com>
2023-04-09 16:14:44 -0500

    Clases (reserva, cuenta)

---
+++ victor/CLASE_cuenta.py
@@ -0,0 +1,31 @@
+class cuenta:
+    def __init__(self, id_cue, id_usu, id_ser, costo_ext):
+        self.__id_cue=id_cue
+        self.__id_usu=id_usu
+        self.__id_ser=id_ser
+        self.costo_ext=costo_ext
+
+    def getId(self):
+        return self.__id_cue
+    
+    def getUsuario(self):
+        return self.__id_usu
+    
+    def getServicio(self):
+        return self.__id_ser
+    
+    def getExtras(self):
+        return self.costo_ext
+    
+    def consultarTodo(self):
+        datos={"Id de la cuenta":self.__id_cue,
+               "Id de el usuario":self.__id_usu,
+               "Id de el servicio":self.__id_ser,
+               "Costos Extras":self.costo_ext}
+        return datos
+    
+    def setExtras(self,new):
+        self.costo_ext=new
+        return self.costo_ext
+    
+ob=cuenta(12,1,69,200.000)
---
+++ victor/CLASE_reservas.py
@@ -0,0 +1,42 @@
+class reserva:
+    def __init__(self, id_res, id_hab, fecha_lleg, fecha_sal, id_usu ):
+        self.__id_res=id_res
+        self.__id_hab=id_hab
+        self.fecha_lleg=fecha_lleg
+        self.fecha_sal=fecha_sal
+        self.__id_usu=id_usu
+
+    def getId(self):
+        return self.__id_res
+    
+    def getHabitacion(self):
+        return self.__id_hab
+    
+    def getLlegada(self):
+        return self.fecha_lleg
+    
+    def getSalida(self):
+        return self.fecha_sal
+    
+    def getUsuario(self):
+        return self.__id_usu
+    
+    def consultarTodo(self):
+        datos={"Id de la reserva":self.__id_res,
+        "Id de la habitacion":self.__id_hab,
+        "Fecha de llegada":self.fecha_lleg,
+        "Fecha de salida":self.fecha_sal,
+        "Id de el usuario":self.__id_usu,}
+        return datos
+    
+    def setLlegada(self,new):
+        self.fecha_lleg=new
+        return self.fecha_lleg
+    
+    def setSalida(self,new):
+        self.fecha_sal=new
+        return self.fecha_sal
+    
+ob=reserva(12345,101,"01/03/2023","15/03/2023",1)
+
+print(ob.consultarTodo())
