342c1c8a82cd709654e1b0a67bdcefac72db1335
angelo <angelorizo84@gmai.com>
2023-04-09 18:31:53 -0500

    Revisar

--- angelo/clase_ser.py
+++ angelo/clase_ser.py
@@ -5,7 +5,7 @@ class servicio:
         self.tipo_ser=tipo_ser
         self.costo_ser=costo_ser
     
-    def consultar_ser(self,id_ser,tipo_ser,costo_ser):
+    def setConsulta(self,id_ser,tipo_ser,costo_ser):
         self.__id_ser=id_ser
         self.tipo_ser=tipo_ser
         self.costo_ser=costo_ser
--- angelo/clases_hab.py
+++ angelo/clases_hab.py
@@ -5,7 +5,7 @@ class habitacione:
         self.tipo_than=tipo_tha
         self.descripcion_hab=descripcion_hab 
     
-    def consultar_hab(self,id_hab,tipo_tha,descripcion_hab):
+    def setConsulta(self,id_hab,tipo_tha,descripcion_hab):
         self.__id_hab=id_hab
         self.tipo_than=tipo_tha
         self.descripcion_hab=descripcion_hab
